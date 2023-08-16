588791a6761e2229664d5fed8ec897f71396a838
nickhuang0128 <44517977+nickhuang0128@users.noreply.github.com>
2023-08-16 12:26:13 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '6296d57c-3ccd-4a27-ae71-25a9ec5ed849'
+export const API_KEY = 'app-shoQQzaobDLkgyoGtXBywMLh'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Text Generator APP',
@@ -15,6 +15,3 @@ export const API_PREFIX = '/api'
 export const LOCALE_COOKIE_NAME = 'locale'
 
 export const DEFAULT_VALUE_MAX_LEN = 48
-
-export const APP_ID = '6296d57c-3ccd-4a27-ae71-25a9ec5ed849'
-export const API_KEY = 'app-shoQQzaobDLkgyoGtXBywMLh'
